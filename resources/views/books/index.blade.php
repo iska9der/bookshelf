5c5e73e117a3a31504a554e973533168329bd29d
Aksi <iskandershamsudinov@gmail.com>
2016-05-24 22:57:44 +0300

    Подправлен стиль вывода книг. Добавлен баннер

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($books as $book)
-        <article>
-            <h2 style="display: inline-block">
-                <a href="{{ url('/books', $book->id) }}">
-                    {{ $book->title }}
-                </a>
-            </h2>
-            <h3 style="display: inline-block; font-size: 12px">{{ $book->published_at }}</h3>
-            <div class="description">{{ $book->description }}</div>
-        </article>
-    @endforeach
+    @include('partials.banner', ['header' => 'Книги', 'body' => 'Список всех существующих книг в нашей базе.'])
+
+    <div class="container">
+        <div class="row">
+            @foreach ($books as $book)
+                <div class="col-md-3">
+                    <h2 style="display: inline-block">
+                        <a href="{{ url('/books', $book->id) }}">
+                            {{ $book->title }}
+                        </a>
+                    </h2>
+                    <h3 style="display: inline-block; font-size: 12px">{{ $book->published_at }}</h3>
+                    <div class="description">{{ $book->description }}</div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @stop
---
+++ resources/views/partials/banner.blade.php
@@ -0,0 +1,8 @@
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="jumbotron__header">{{ $header }}</h1>
+        <p class="jumbotron__body">
+            {{ $body }}
+        </p>
+    </div>
+</div>
